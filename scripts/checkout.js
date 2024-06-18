56e44c798e1ec732cca8b3cf6fd53e3975653e7a
AnuprabhaB <129923053+AnuprabhaB@users.noreply.github.com>
2024-06-18 18:05:41 +0530

    Delete scripts directory

--- scripts/checkout.js
+++
@@ -1,7 +0,0 @@
-import { renderOrderSummary } from "./Checkout/orderSummary.js";
-import { renderPaymentSummary } from "./Checkout/paymentSummary.js";
-//import '../data/cart-class.js';
-import '../data/backend-practice.js';
-
-renderOrderSummary();
-renderPaymentSummary();
--- scripts/utils/money.js
+++
@@ -1,5 +0,0 @@
-export function formatCurrency(priceCents){
-  return (Math.round(priceCents)/100).toFixed(2);
-}
-//the below makes it imported into the file in default when we export it
-export default formatCurrency;
